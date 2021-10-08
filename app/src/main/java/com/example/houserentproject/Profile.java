fd326b475d1ec55abc1bebdb4f10225fa4e47c22
Shuvo Talukdar <shuvo6904@gmail.com>
2021-10-08 08:37:55 +0600

    store profile image to firebase

--- app/src/main/java/com/example/houserentproject/Profile.java
+++ app/src/main/java/com/example/houserentproject/Profile.java
@@ -1,26 +1,40 @@
 package com.example.houserentproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class Profile extends AppCompatActivity {
 
     private Button profileEmailVerifyButton, editProfileButton;
     private BottomSheetDialog sheetDialog;
     private FirebaseAuth fAuth;
+    private ImageView profileImage;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,9 @@ public class Profile extends AppCompatActivity {
 
         profileEmailVerifyButton = (Button) findViewById(R.id.verifiedEmailButtonId);
         editProfileButton = (Button) findViewById(R.id.editProfileButtonId);
+        profileImage = (ImageView) findViewById(R.id.editableProfileImageViewId);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         fAuth = FirebaseAuth.getInstance();
 
@@ -85,5 +102,56 @@ public class Profile extends AppCompatActivity {
         });
 
 
+    }
+
+    public void fabChangeProPic(View view) {
+
+        ImagePicker.with(Profile.this)
+                .cameraOnly()	//User can only capture image using Camera
+                .crop()	    			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start(1000);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000){
+            if (resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                //profileImage.setImageURI(imageUri);
+
+                uploadImageToFirebase(imageUri);
+            }
+        }
+
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        // upload image to firebase storage
+        StorageReference fileRef = storageReference.child("profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        Picasso.get().load(uri).into(profileImage);
+
+                    }
+                });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 }
